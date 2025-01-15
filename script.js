f2bdf584f6e363709e9394cd77040fb8c3d898fa
Hichem ben ayed <benayedhicham@gmail.com>
2025-01-15 11:23:27 +0100

    Début

---
+++ script.js
@@ -0,0 +1,13 @@
+// Les variables :
+let sp, btn_start, btn_stop, t, ms, s, mn, h;
+
+// Fonction pour initialiser les variable quand la page se charge
+
+window.onload = function(){
+    sp = document.getElementsByTagName("span");
+    btn_start = document.getElementById("start");
+    btn_stop = document.getElementById("stop");
+    t;
+    ms = 0, s = 0, mn = 0, h = 0 ;
+}
+
